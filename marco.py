78e8b14ab4165d2c6a89b6c7f349d892873d1638
Angelo Kiriakoulis <angelokir86@gmail.com>
2023-05-16 13:38:39 +0300

    Reorganized the marco algorithm into a class

--- marco.py
+++ marco.py
@@ -1,108 +1,107 @@
 from pysat.solvers import Glucose3
 
-# Parse the constraint set in CNF form using the pySAT library
-# cnf = [[1],[-1],[2],[-2],[1,2]]
-# cnf = [[-1,2],[-2,3],[-3]]
-# cnf = [[-1,2], [1,-2], [3], [-1,-3], [2,-3], [-3]]
-# cnf = [[-1,2],[-2,3],[-1],[-3],[2]]
-# cnf = [[1, 2, 3], [-1, -2], [2, 3], [-3, -1], [1], [2]]
-cnf = [[1],[-1,2],[-2],[-1]]
-
-# Compute one minimum unsatisfiability sub-set from a random unsatisfiable constraint subset that we input.
-def shrink(seed):
+class Marco:
+  def __init__(self,cnf) -> None:
+    self.cnf = cnf
+    self.Map=[[i+1 for i in range(len(self.cnf))]]
+    self.mus_list = [] 
+    self.mss_list = []
+    self.find_subsets()
+  
+  def shrink(self, seed):
     for c in seed[:]:
-        seed_copy = seed.copy()
-        seed.remove(c)
-        solver = Glucose3()
-        for clause in seed:
-            solver.add_clause(clause)
-        if solver.solve():
-            seed = seed_copy
+      seed_copy = seed.copy()
+      seed.remove(c)
+      solver = Glucose3()
+      for clause in seed:
+        solver.add_clause(clause)
+      if solver.solve():
+        seed = seed_copy
     solver.delete()
     return seed
-
-# Compute one maximum satisfiability sub-set from a random unsatisfiable constraint subset that we input.
-def grow(seed):
-    for c in cnf:
-        while True:
-            if c not in seed:
-                solver = Glucose3()
-                for clause in seed:
-                    solver.add_clause(clause)
-                solver.add_clause(c)
-                if solver.solve():
-                    seed += [c]
-                else: break
-            else: break
-            solver.delete()
+  
+  def grow(self, seed):
+    for c in self.cnf:
+      while True:
+        if c not in seed:
+          solver = Glucose3()
+          for clause in seed:
+            solver.add_clause(clause)
+          solver.add_clause(c)
+          if solver.solve():
+            seed += [c]
+          else: break
+        else:break
+        solver.delete()
     return seed
+  
+  def block_up(self, mus):
+    block_up_list = []
+    for i in mus:
+      block_up_list.append(-self.cnf.index(i) - 1)
+    return block_up_list
+  
+  def block_down(self, mss):
+    block_down_list = []
+    for i in self.cnf:
+      if i not in mss:
+        block_down_list.append(self.cnf.index(i) + 1)
+    return block_down_list
 
-def blockUp(mus):
-    blockup_list=[]
-    for i in mus: blockup_list.append(-cnf.index(i)-1)
-    return blockup_list
-
-def blockDown(mss):
-    blockdown_list=[]
-    for i in cnf: 
-        if i not in mss:
-            blockdown_list.append(cnf.index(i)+1)
-    return blockdown_list
-
-def getUnexplored(Map):
+  def get_unexplored(self, Map):
     solver = Glucose3()
     highest_num_positives = 0
     lists_with_highest_num_positives = []
     f = []
     for clause in Map:
-        solver.add_clause(clause)
+      solver.add_clause(clause)
     solver.solve()
-    # Iterate over each list
     for formula in solver.enum_models():
-        # Count the number of positive values in the list
-        num_positives = len(list(filter(lambda x: x > 0, formula)))
-        if num_positives > highest_num_positives:
-            highest_num_positives = num_positives
-            lists_with_highest_num_positives = [formula]
-        elif num_positives == highest_num_positives:
-            lists_with_highest_num_positives.append(formula)
-        break
+      num_positives = len(list(filter(lambda x: x > 0, formula)))
+      if num_positives > highest_num_positives:
+        highest_num_positives = num_positives
+        lists_with_highest_num_positives = [formula]
+      elif num_positives == highest_num_positives:
+        lists_with_highest_num_positives.append(formula)
+      break
     for formula in lists_with_highest_num_positives:
-        s = []
-        for i in formula:
-            if i>0:s.append(cnf[i-1])
-        f.append(s)
+      s = []
+      for i in formula:
+        if i > 0: s.append(self.cnf[i - 1])
+      f.append(s)
     return f
-
-def isSatifiable(problem):
+  
+  def is_satisfiable(self, problem):
     solver = Glucose3()
     for clause in problem:
-        solver.add_clause(clause)
+      solver.add_clause(clause)
     flag = solver.solve()
     return flag
-
-Map=[[i+1 for i in range(len(cnf))]]
-mus_list = [] 
-mss_list = []
-while True:
-    seed_list = getUnexplored(Map)
-    for seed in seed_list:
-        if isSatifiable(seed):
-            mss = grow(seed)
-            if mss not in mss_list:
-                mss_list.append(mss)
-                Map.append(blockDown(mss))
-        else:            
-            mus = shrink(seed)
-            if mus not in mus_list: 
-                mus_list.append(mus)
-                Map.append(blockUp(mus))
+  
+  def find_subsets(self):
+    while True:
+      seed_list = self.get_unexplored(self.Map)
+      for seed in seed_list:
+        if self.is_satisfiable(seed):
+          mss = self.grow(seed)
+          if mss not in self.mss_list:
+            self.mss_list.append(mss)
+            self.Map.append(self.block_down(mss))
+        else:
+          mus = self.shrink(seed)
+          if mus not in self.mus_list:
+            self.mus_list.append(mus)
+            self.Map.append(self.block_up(mus))
         seed_list.remove(seed)
-    if not isSatifiable(Map): 
+      if not self.is_satisfiable(self.Map):
         break
-print("Maximum Satisfiable Subsets:")
-for i in mss_list:
-    print(i)
-print("Minimum Unsatisfiable Subsets:")
-for i in mus_list:
-    print(i)
+    print("Maximum Satisfiable Subsets:")
+    for i in self.mss_list:
+      print(i)
+    print("Minimum Unsatisfiable Subsets:")
+    for i in self.mus_list:
+      print(i)
+
+
+cnf = [[1],[2],[-1,-2]]
+marco = Marco(cnf)
