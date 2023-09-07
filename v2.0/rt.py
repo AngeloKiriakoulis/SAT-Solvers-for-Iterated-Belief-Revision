ae971f841008a4e5a5422b5d0cb2ecff519ec081
AngeloKiriakoulis <angelokir86@gmail.com>
2023-09-07 17:51:50 +0300

    fixed all the bugs in negate,due to np refactorin

---
+++ v2.0/rt.py
@@ -0,0 +1,5 @@
+import numpy as np
+
+# Your original NumPy array
+original_array = np.array([list([-15, -33, 43, 54]),list([-15, -33, 43]), list([-3, -26, -69]), list([-36, -49, -95]), list([4, -44, -47])],dtype=object)
+
